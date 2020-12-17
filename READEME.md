d7857f0b28c6df074e3f59c6fbbaf48c2b01bb69
jimmy <web3d@live.com>
2020-12-17 11:49:28 +0800

    init 0.1.0, code from yii v2.0.38

---
+++ READEME.md
@@ -0,0 +1,9 @@
+# yiilite-core
+
+将yii2框架中最核心的面向对象和组件思想的代码剥离出来，便于在其它项目中复用
+
+ * yiilite\BaseObject
+ * yiilite\Component
+ * yiilite\Event
+ * yiilite\Behavior
+ * yiilite\di\(依赖)
---
+++ src/Helper.php
@@ -0,0 +1,207 @@
+<?php
+
+namespace yiilite;
+
+use yiilite\di\Container;
+
+class Helper
+{
+    /**
+     * @var Container the dependency injection (DI) container used by [[createObject()]].
+     * You may use [[Container::set()]] to set up the needed dependencies of classes and
+     * their initial property values.
+     * @see createObject()
+     * @see Container
+     */
+    public static $container;
+
+    /**
+     * Creates a new object using the given configuration.
+     *
+     * You may view this method as an enhanced version of the `new` operator.
+     * The method supports creating an object based on a class name, a configuration array or
+     * an anonymous function.
+     *
+     * Below are some usage examples:
+     *
+     * ```php
+     * // create an object using a class name
+     * $object = Yii::createObject('yii\db\Connection');
+     *
+     * // create an object using a configuration array
+     * $object = Yii::createObject([
+     *     'class' => 'yii\db\Connection',
+     *     'dsn' => 'mysql:host=127.0.0.1;dbname=demo',
+     *     'username' => 'root',
+     *     'password' => '',
+     *     'charset' => 'utf8',
+     * ]);
+     *
+     * // create an object with two constructor parameters
+     * $object = \Yii::createObject('MyClass', [$param1, $param2]);
+     * ```
+     *
+     * Using [[\yii\di\Container|dependency injection container]], this method can also identify
+     * dependent objects, instantiate them and inject them into the newly created object.
+     *
+     * @param string|array|callable $type the object type. This can be specified in one of the following forms:
+     *
+     * - a string: representing the class name of the object to be created
+     * - a configuration array: the array must contain a `class` element which is treated as the object class,
+     *   and the rest of the name-value pairs will be used to initialize the corresponding object properties
+     * - a PHP callable: either an anonymous function or an array representing a class method (`[$class or $object, $method]`).
+     *   The callable should return a new instance of the object being created.
+     *
+     * @param array $params the constructor parameters
+     * @return object the created object
+     * @throws InvalidConfigException if the configuration is invalid.
+     * @see \yiilite\di\Container
+     */
+    public static function createObject($type, array $params = [])
+    {
+        if (is_string($type)) {
+            return static::$container->get($type, $params);
+        }
+
+        if (is_callable($type, true)) {
+            return static::$container->invoke($type, $params);
+        }
+
+        if (!is_array($type)) {
+            throw new InvalidConfigException('Unsupported configuration type: ' . gettype($type));
+        }
+
+        if (isset($type['__class'])) {
+            $class = $type['__class'];
+            unset($type['__class'], $type['class']);
+            return static::$container->get($class, $params, $type);
+        }
+
+        if (isset($type['class'])) {
+            $class = $type['class'];
+            unset($type['class']);
+            return static::$container->get($class, $params, $type);
+        }
+
+        throw new InvalidConfigException('Object configuration must be an array containing a "class" or "__class" element.');
+    }
+
+    /**
+     * Configures an object with the initial property values.
+     * @param object $object the object to be configured
+     * @param array $properties the property initial values given in terms of name-value pairs.
+     * @return object the object itself
+     */
+    public static function configure($object, $properties)
+    {
+        foreach ($properties as $name => $value) {
+            $object->$name = $value;
+        }
+
+        return $object;
+    }
+
+    /**
+     * Returns the public member variables of an object.
+     * This method is provided such that we can get the public member variables of an object.
+     * It is different from "get_object_vars()" because the latter will return private
+     * and protected variables if it is called within the object itself.
+     * @param object $object the object to be handled
+     * @return array the public member variables of the object
+     */
+    public static function getObjectVars($object)
+    {
+        return get_object_vars($object);
+    }
+
+    /**
+     * Returns a value indicating whether the given array is an associative array.
+     *
+     * An array is associative if all its keys are strings. If `$allStrings` is false,
+     * then an array will be treated as associative if at least one of its keys is a string.
+     *
+     * Note that an empty array will NOT be considered associative.
+     *
+     * @param array $array the array being checked
+     * @param bool $allStrings whether the array keys must be all strings in order for
+     * the array to be treated as associative.
+     * @return bool whether the array is associative
+     */
+    public static function isAssociative($array, $allStrings = true)
+    {
+        if (!is_array($array) || empty($array)) {
+            return false;
+        }
+
+        if ($allStrings) {
+            foreach ($array as $key => $value) {
+                if (!is_string($key)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        foreach ($array as $key => $value) {
+            if (is_string($key)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks if the passed string would match the given shell wildcard pattern.
+     * This function emulates [[fnmatch()]], which may be unavailable at certain environment, using PCRE.
+     * @param string $pattern the shell wildcard pattern.
+     * @param string $string the tested string.
+     * @param array $options options for matching. Valid options are:
+     *
+     * - caseSensitive: bool, whether pattern should be case sensitive. Defaults to `true`.
+     * - escape: bool, whether backslash escaping is enabled. Defaults to `true`.
+     * - filePath: bool, whether slashes in string only matches slashes in the given pattern. Defaults to `false`.
+     *
+     * @return bool whether the string matches pattern or not.
+     * @since 2.0.14
+     */
+    public static function matchWildcard($pattern, $string, $options = [])
+    {
+        if ($pattern === '*' && empty($options['filePath'])) {
+            return true;
+        }
+
+        $replacements = [
+            '\\\\\\\\' => '\\\\',
+            '\\\\\\*' => '[*]',
+            '\\\\\\?' => '[?]',
+            '\*' => '.*',
+            '\?' => '.',
+            '\[\!' => '[^',
+            '\[' => '[',
+            '\]' => ']',
+            '\-' => '-',
+        ];
+
+        if (isset($options['escape']) && !$options['escape']) {
+            unset($replacements['\\\\\\\\']);
+            unset($replacements['\\\\\\*']);
+            unset($replacements['\\\\\\?']);
+        }
+
+        if (!empty($options['filePath'])) {
+            $replacements['\*'] = '[^/\\\\]*';
+            $replacements['\?'] = '[^/\\\\]';
+        }
+
+        $pattern = strtr(preg_quote($pattern, '#'), $replacements);
+        $pattern = '#^' . $pattern . '$#us';
+
+        if (isset($options['caseSensitive']) && !$options['caseSensitive']) {
+            $pattern .= 'i';
+        }
+
+        return preg_match($pattern, $string) === 1;
+    }
+}
